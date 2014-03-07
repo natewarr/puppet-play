e17d9d685990b716b933092bdc8defa99814d722
Nate Warren <nate.warren@lifeway.org>
2014-03-06 22:24:14 -0600

    fix service start and requires

--- manifests/application.pp
+++ manifests/application.pp
@@ -15,58 +15,60 @@ define play::application(
   Exec { user => 'play'}
 
   require play
-    case $ensure {
-      'installed' : {
+  case $ensure {
+    'installed' : {
 
-        #download the application from $source
-        wget::fetch { $source:
-          destination => "${path}/cache/zip/${app_name}-${version}.zip",
-        } ->
+      #download the application from $source
+      wget::fetch { $source:
+        destination => "${path}/cache/zip/${app_name}-${version}.zip",
+      } ->
 
-        #extract application archive
-        exec {"extract ${source}":
-          command => "/usr/bin/unzip ${path}/cache/zip/${app_name}-${version}.zip -d ${path}/apps",
-          path    => "${path}/apps",
-          creates => "${path}/apps/${app_name}-${version}",
-        } ->
+      #extract application archive
+      exec {"extract ${source}":
+        command => "/usr/bin/unzip ${path}/cache/zip/${app_name}-${version}.zip -d ${path}/apps",
+        path    => "${path}/apps",
+        creates => "${path}/apps/${app_name}-${version}",
+      } ->
 
-        file {"${path}/apps/${app_name}-${version}" :
-          ensure => 'directory',
+      file {"${path}/apps/${app_name}-${version}" :
+        ensure => 'directory',
+      }
+
+      file {"${path}/conf/${service_name}.conf":
+        ensure => 'file',
+        source => $config_file,
+        owner  => 'play',
+        group  => 'play',
+        mode   => '0600',
+      }
+
+      file {"${path}/logs/${service_name}" :
+        ensure => directory,
+        owner  => 'play',
+        group  => 'play',
+        mode   => '0755',
+      }
+
+      if ( $service == true ) {
+        file {"/etc/init.d/${service_name}":
+          ensure  => 'file',
+          content => template('play/initd-play-app.erb'),
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0755',
+          require => File["${path}/conf/${service_name}.conf",
+                          "${path}/logs/${service_name}" ],
+          notify => Service[$service_name],
         }
 
-        file {"${path}/conf/${service_name}.conf":
-          ensure => 'file',
-          source => $config_file,
-          owner  => 'play',
-          group  => 'play',
-          mode   => '0600',
+        service { $service_name :
+          ensure     => 'running',
+          enable     => $enable,
+          hasstatus  => true,
+          hasrestart => true,
+          require    => File["/etc/init.d/${service_name}"],
         }
-
-        file {"${path}/logs/${service_name}" :
-          ensure => directory,
-          owner  => 'play',
-          group  => 'play',
-          mode   => '0755',
-        }
-
-        if ( $service == true ) {
-          file {"/etc/init.d/${service_name}":
-            ensure  => 'file',
-            content => template('play/initd-play-app.erb'),
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0755',
-            require => File["${path}/conf/${service_name}.conf",
-                            "${path}/logs/${service_name}" ],
-          } ->
-
-          service { $service_name :
-            ensure     => running,
-            enable     => $enable,
-            hasstatus  => true,
-            hasrestart => true,
-          }
-        }
+      }
     }
     'absent': {
       service {"play-${service_name}":
