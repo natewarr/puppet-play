7121ba0814c0c706197d024a5aade5cd6054d484
Nate Warren <nate.warren@lifeway.com>
2014-08-14 13:07:20 -0500

    try backticks

--- manifests/cleanup.pp
+++ manifests/cleanup.pp
@@ -6,14 +6,14 @@ class play::cleanup (
   $keepCurrent = $cacheHistory + 1
 
   exec { 'cleanup-cache-zips':
-    command => "count=$(ls . | wc -l); delete=$(expr ${count} - ${keepCurrent}); ls -tr . | head -n ${delete} | xargs rm -f",
+    command => "count=`ls . | wc -l`; delete=`expr ${count} - ${keepCurrent}`; ls -tr . | head -n ${delete} | xargs rm -f",
     cwd     => "${homePath}/cache/zip",
     path    => ['/bin','/usr/bin','/usr/local/bin'],
     user    => 'play',
   }
 
   exec { 'cleanup-cache-dirs':
-    command => "count=$(ls . | wc -l); delete=$(expr ${count} - ${keepCurrent}); ls -tr . | head -n ${delete} | xargs rm -rf",
+    command => "count=`ls . | wc -l`; delete=`expr ${count} - ${keepCurrent}`; ls -tr . | head -n ${delete} | xargs rm -rf",
     cwd     => "$homePath/apps",
     path    => ['/bin','/usr/bin','/usr/local/bin'],
     user    => 'play',
