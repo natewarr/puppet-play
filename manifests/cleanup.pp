5ec9ac293d63a7164a22e771f80c06d8091e0f43
Nate Warren <nate.warren@lifeway.com>
2014-08-14 12:57:06 -0500

    fix cleanup

--- manifests/cleanup.pp
+++ manifests/cleanup.pp
@@ -6,15 +6,15 @@ class play::cleanup (
   $keepCurrent = $cacheHistory + 1
 
   exec { 'cleanup-cache-zips':
-    command => "ls -t $homePath/cache/zip | head -n $cacheHistory|sort|uniq -u|xargs rm -f",
-    cwd     => $homePath,
+    command => "count=$(ls . | wc -l); delete=$(expr ${count} - ${keepCurrent}); ls -tr . | head -n ${delete} | xargs rm -f",
+    cwd     => "${homePath}/cache/zip",
     path    => ['/bin','/usr/bin','/usr/local/bin'],
     user    => 'play',
   }
 
   exec { 'cleanup-cache-dirs':
-    command => "ls -t $homePath/apps | head -n $keepCurrent|sort|uniq -u|xargs rm -rf",
-    cwd     => $homePath,
+    command => "count=$(ls . | wc -l); delete=$(expr ${count} - ${keepCurrent}); ls -tr . | head -n ${delete} | xargs rm -rf",
+    cwd     => "$homePath/apps",
     path    => ['/bin','/usr/bin','/usr/local/bin'],
     user    => 'play',
   }
